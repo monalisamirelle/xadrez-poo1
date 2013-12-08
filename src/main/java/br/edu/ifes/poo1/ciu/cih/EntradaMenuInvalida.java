c909ed5bda887319447ee3b7ed793b74c24eb2de
possatti <lucas_possatti@hotmail.com>
2013-12-08 17:20:50 -0200

    Iniciado o desenvolvimento da interface, tanto da Camada de Interação Humana (cih), quanto da Camada de Controle de Interação (cci).

---
+++ src/main/java/br/edu/ifes/poo1/ciu/cih/EntradaMenuInvalida.java
@@ -0,0 +1,17 @@
+package br.edu.ifes.poo1.ciu.cih;
+
+/**
+ * Excessão lançada quando o usuário seleciona uma entrada do menu principal
+ * inválida.
+ * 
+ * @author lucas
+ * 
+ */
+public class EntradaMenuInvalida extends Exception {
+
+	/**
+	 * Número de série, gerado automaticamente.
+	 */
+	private static final long serialVersionUID = -7422850167962510604L;
+
+}
