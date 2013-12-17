f211a103eff79bebfa31f911b9841f410945f770
thanner <thannersoares@gmail.com>
2013-12-17 15:22:30 -0200

    Peao fica comentado por enquanto

--- src/main/java/br/edu/ifes/poo1/cln/cdp/Peao.java
+++ src/main/java/br/edu/ifes/poo1/cln/cdp/Peao.java
@@ -16,33 +16,35 @@ public class Peao extends Peca {
 	@Override
 	public boolean podeAndar(Posicao origem, Posicao destino,
 			Tabuleiro tabuleiro) {
-		// FIXME: PEÃO BRANCO ANDA +1 E PEÃO PRETO ANDA -1
-		if (super.podeAndar(origem, destino, tabuleiro)
-				&& tabuleiro.podeRealizarMovimentacao(origem, destino))
-			// Se o peão quer avançar, não quer se movimentar na coluna
-			if ((origem.getLinha() - destino.getLinha() > 0)
-					& (origem.getColuna() - destino.getColuna() == 0)) {
-				// Se o peão já se movimentou alguma vez
-				if (this.getJaMoveu() == true) {
-					if ((origem.getLinha() - destino.getLinha()) == 1)
-						return true;
-				} else {
-					if ((origem.getLinha() - destino.getLinha()) < 3)
-						return true;
-				}
-			}
-		return false;
+		// FIXME: PEÃO BRANCO +1 PEÃO PRETO -1
+		// if (super.podeAndar(origem, destino, tabuleiro)
+		// && tabuleiro.podeRealizarMovimentacao(origem, destino))
+		// Se o peão quer avançar, não quer se movimentar na coluna
+		// if ((origem.getLinha() - destino.getLinha() > 0)
+		// & (origem.getColuna() - destino.getColuna() == 0)) {
+		// Se o peão já se movimentou alguma vez
+		// if (this.getJaMoveu() == true) {
+		// if ((origem.getLinha() - destino.getLinha()) == 1)
+		// return true;
+		// } else {
+		// if ((origem.getLinha() - destino.getLinha()) < 3)
+		// return true;
+		// }
+		// }
+		// return false;
+		return true;
 	}
 
 	@Override
 	public boolean podeAtacar(Posicao origem, Posicao destino,
 			Tabuleiro tabuleiro) {
-		if (super.podeAtacar(origem, destino, tabuleiro)
-				&& tabuleiro.podeRealizarMovimentacao(origem, destino))
-			if ((origem.getLinha() - destino.getLinha() == 1)
-					&& (this.tamanhoMovimento(origem.getColuna(),
-							destino.getColuna()) == 1))
-				return true;
-		return false;
+		// if (super.podeAtacar(origem, destino, tabuleiro)
+		// && tabuleiro.podeRealizarMovimentacao(origem, destino))
+		// if ((origem.getLinha() - destino.getLinha() == 1)
+		// && (this.tamanhoMovimento(origem.getColuna(),
+		// destino.getColuna()) == 1))
+		// return true;
+		// return false;
+		return true;
 	}
 }
