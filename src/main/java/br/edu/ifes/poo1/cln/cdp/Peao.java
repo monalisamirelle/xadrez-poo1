515ca334f78442d86475170fdc98f6e98ce5a29d
thanner <thannersoares@gmail.com>
2013-12-17 21:46:41 -0200

    Pronto Agora temos: definir jogadas especiais + melhorar código

--- src/main/java/br/edu/ifes/poo1/cln/cdp/Peao.java
+++ src/main/java/br/edu/ifes/poo1/cln/cdp/Peao.java
@@ -16,35 +16,43 @@ public class Peao extends Peca {
 	@Override
 	public boolean podeAndar(Posicao origem, Posicao destino,
 			Tabuleiro tabuleiro) {
-		// FIXME: PEÃO BRANCO +1 PEÃO PRETO -1
-		// if (super.podeAndar(origem, destino, tabuleiro)
-		// && tabuleiro.podeRealizarMovimentacao(origem, destino))
-		// Se o peão quer avançar, não quer se movimentar na coluna
-		// if ((origem.getLinha() - destino.getLinha() > 0)
-		// & (origem.getColuna() - destino.getColuna() == 0)) {
-		// Se o peão já se movimentou alguma vez
-		// if (this.getJaMoveu() == true) {
-		// if ((origem.getLinha() - destino.getLinha()) == 1)
-		// return true;
-		// } else {
-		// if ((origem.getLinha() - destino.getLinha()) < 3)
-		// return true;
-		// }
-		// }
-		// return false;
-		return true;
+		int avanca;
+		if ((this.getJogador().getCor().toString()).equals("BRANCO"))
+			avanca = 1;
+		else
+			avanca = -1;
+		if (super.podeAndar(origem, destino, tabuleiro)
+				&& tabuleiro.podeRealizarMovimentacao(origem, destino))
+			// Se o peão quer avançar, não quer se movimentar na coluna
+			if (origem.getColuna() - destino.getColuna() == 0) {
+				// Se o peão já se movimentou alguma vez
+				if (this.getJaMoveu() == true) {
+					if ((destino.getLinha() - origem.getLinha()) == avanca)
+						return true;
+				} else {
+					if (destino.getLinha() - origem.getLinha() == avanca
+							|| destino.getLinha() - origem.getLinha() == 2 * avanca)
+						return true;
+				}
+			}
+		return false;
 	}
 
 	@Override
 	public boolean podeAtacar(Posicao origem, Posicao destino,
 			Tabuleiro tabuleiro) {
-		// if (super.podeAtacar(origem, destino, tabuleiro)
-		// && tabuleiro.podeRealizarMovimentacao(origem, destino))
-		// if ((origem.getLinha() - destino.getLinha() == 1)
-		// && (this.tamanhoMovimento(origem.getColuna(),
-		// destino.getColuna()) == 1))
-		// return true;
-		// return false;
-		return true;
+		int avanca;
+		if ((this.getJogador().getCor().toString()).equals("BRANCO"))
+			avanca = 1;
+		else
+			avanca = -1;
+		if (super.podeAtacar(origem, destino, tabuleiro)
+				&& tabuleiro.podeRealizarMovimentacao(origem, destino))
+			// Se quer avançar na coluna 1
+			if(this.tamanhoMovimento(origem.getColuna(),
+							destino.getColuna()) == 1)
+				if(destino.getLinha() - origem.getLinha() == avanca)
+					return true;
+		return false;
 	}
 }
