ae158b836fbd37181b2cc7ea9d1dbc00281bfe45
possatti <lucas_possatti@hotmail.com>
2013-12-21 13:15:45 -0200

    Faltou submeter a nova enum 'Tipojogada' no último commit.

---
+++ src/main/java/br/edu/ifes/poo1/cln/cdp/TipoJogada.java
@@ -0,0 +1,8 @@
+package br.edu.ifes.poo1.cln.cdp;
+
+/**
+ * Enumera os tipos de jogadas possíveis de se fazer para o jogo de xadrez.
+ */
+public enum TipoJogada {
+	ANDAR, ATACAR, ROQUE_MENOR, ROQUE_MAIOR
+}
